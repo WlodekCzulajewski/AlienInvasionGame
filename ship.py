b2b3144393cebe996b0affee5aee1c302b5da4bd
Włodek <wlodekczulajewski@gmail.com>
2024-03-07 10:59:03 +0100

    Refactor check events function and add quit by key

--- ship.py
+++ ship.py
@@ -28,9 +28,9 @@ class Ship:
     def update(self):
         """Actualization of ships position based on option of movement."""
         #Actualization of ships X location, but not its rect.
-        if self.moving_right:
+        if self.moving_right and self.rect.right < self.screen_rect.right:
             self.x += self.settings.ship_speed
-        if self.moving_left:
+        if self.moving_left and self.rect.left > 0:
             self.x -= self.settings.ship_speed
         
         #Actualization of rect X location, based on value of self x.
