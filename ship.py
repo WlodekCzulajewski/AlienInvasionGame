5df1c686e83924d1cc9aa1eab90bbb94a3eed8a1
Włodek <wlodekczulajewski@gmail.com>
2024-03-07 10:42:36 +0100

    Add left movement and ships speed setting

--- ship.py
+++ ship.py
@@ -8,6 +8,7 @@ class Ship:
         """Initialization of spaceship and its starting position."""
 
         self.screen = ai_game.screen
+        self.settings = ai_game.settings
         self.screen_rect = ai_game.screen.get_rect()
 
         #Load spaceship image and load its rectangle.
@@ -17,13 +18,23 @@ class Ship:
         #Every new spaceship starts at the bottom of a screen.
         self.rect.midbottom = self.screen_rect.midbottom
     
+        #Position of spaceship is saved in float type.
+        self.x = float(self.rect.x)
+
         #Options of movement
         self.moving_right = False
+        self.moving_left = False
 
     def update(self):
         """Actualization of ships position based on option of movement."""
+        #Actualization of ships X location, but not its rect.
         if self.moving_right:
-            self.rect.x += 1
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+        
+        #Actualization of rect X location, based on value of self x.
+        self.rect.x = self.x
 
     def blitme(self):
         """Display spaceship at his current position."""
